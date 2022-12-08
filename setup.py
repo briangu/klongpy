caa1fdb83c637de2d160d31868c1b548d3ab06bd
Brian Guarraci <brian.guarraci@gmail.com>
2022-12-08 09:07:46 -0700

    update version to 0.3.48

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
 setup(
     name='klongpy',
     packages=['klongpy'],
-    version='0.3.47',
+    version='0.3.48',
     description='Python implementation of Klong language.',
     author='Brian Guarraci',
     license='MIT',
