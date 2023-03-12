1c208265cdbcf1988ce76765503475ccc65477a7
Brian Guarraci <brian.guarraci@gmail.com>
2023-03-12 15:28:51 -0600

    fix nparray

--- klongpy/adverbs.py
+++ klongpy/adverbs.py
@@ -233,7 +233,7 @@ def eval_adverb_over(f, a):
                 return np.add.reduce(a)
         elif safe_eq(wrapped.a, '-'):
             if len(a.shape) == 1:
-                return np.sum([a[0],*(a[1:]*-1)])
+                return np.sum(np.asarray([a[0],*(a[1:]*-1)]))
             elif hasattr(np.subtract,'reduce'):
                 return np.subtract.reduce(a)
         elif safe_eq(wrapped.a, '*') and hasattr(np.multiply,'reduce'):
