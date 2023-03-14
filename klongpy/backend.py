c48a3a66066c081147ad240257502d50f20be67b
Brian Guarraci <brian.guarraci@gmail.com>
2023-03-14 16:15:02 -0600

    use better kernel

--- klongpy/backend.py
+++ klongpy/backend.py
@@ -4,6 +4,7 @@ import warnings
 use_gpu = bool(os.environ.get('USE_GPU') == '1')
 
 if use_gpu:
+    import cupy as cp
     import cupy as np
     import numpy
 
@@ -19,24 +20,48 @@ if use_gpu:
         def reduce(self, x):
             return self.reduce_fn_1(x) if len(x.shape) == 1 else self.reduce_fn_2(x[0], x[1])
 
-    add_reduce_2 = np.ElementwiseKernel(
+    add_reduce_2 = cp.ElementwiseKernel(
             'T x, T y',
             'T z',
             'z = (x + y)',
             'add_reduce_2')
-    np.add = CuPyReductionKernelWrapper(np.add, np.sum, add_reduce_2)
+    cp.add = CuPyReductionKernelWrapper(cp.add, cp.sum, add_reduce_2)
 
-    def subtract_reduce_1(x):
-        return 2*x[0] - np.sum(x)
+    # def subtract_reduce_1(x):
+    #     return 2*x[0] - cp.sum(x)
 
-    subtract_reduce_2 = np.ElementwiseKernel(
+    subtract_reduce_kernel_1d = cp.RawKernel(r'''
+        extern "C" __global__
+        void subtract_reduce_kernel_1d(const float* input, float* output, const int length) {
+            int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+            if (idx < length) {
+                float value = (idx == 0) ? input[idx] : -input[idx];
+                atomicAdd(output, value);
+            }
+        }
+        ''', 'subtract_reduce_kernel_1d')
+
+    def subtract_reduce_1(arr):
+        input_arr = cp.asarray(arr)
+        length = input_arr.size
+        output_arr = cp.zeros(1, dtype=input_arr.dtype)
+
+        threads_per_block = 256
+        blocks_per_grid = (length + threads_per_block - 1) // threads_per_block
+
+        subtract_reduce_kernel_1d((blocks_per_grid,), (threads_per_block,), (input_arr, output_arr, length))
+
+        return output_arr
+
+    subtract_reduce_2 = cp.ElementwiseKernel(
             'T x, T y',
             'T z',
             'z = (x - y)',
             'subtract_reduce_2')
-    np.subtract = CuPyReductionKernelWrapper(np.subtract, subtract_reduce_1, subtract_reduce_2)
+    np.subtract = CuPyReductionKernelWrapper(cp.subtract, subtract_reduce_1, subtract_reduce_2)
 
-    multiply_reduce_1 = np.ReductionKernel(
+    multiply_reduce_1 = cp.ReductionKernel(
                 'T x',
                 'T y',
                 'x',
@@ -45,14 +70,14 @@ if use_gpu:
                 '1',
                 'multiply_reduce_1'
              )
-    multiply_reduce_2 = np.ElementwiseKernel(
+    multiply_reduce_2 = cp.ElementwiseKernel(
             'T x, T y',
             'T z',
             'z = (x * y)',
             'multiply_reduce_2')
-    np.multiply = CuPyReductionKernelWrapper(np.multiply, multiply_reduce_1, multiply_reduce_2)
+    np.multiply = CuPyReductionKernelWrapper(cp.multiply, multiply_reduce_1, multiply_reduce_2)
 
-    divide_reduce_1 = np.ReductionKernel(
+    divide_reduce_1 = cp.ReductionKernel(
                 'T x',
                 'T y',
                 'x',
@@ -61,14 +86,14 @@ if use_gpu:
                 '1',
                 'divide_reduce_1'
              )
-    divide_reduce_2 = np.ElementwiseKernel(
+    divide_reduce_2 = cp.ElementwiseKernel(
             'T x, T y',
             'T z',
             'z = (x / y)',
             'divide_reduce_2')
-    np.divide = CuPyReductionKernelWrapper(np.divide, divide_reduce_1, divide_reduce_2)
+    np.divide = CuPyReductionKernelWrapper(cp.divide, divide_reduce_1, divide_reduce_2)
 
-    np.isarray = lambda x: isinstance(x, (numpy.ndarray, np.ndarray))
+    np.isarray = lambda x: isinstance(x, (numpy.ndarray, cp.ndarray))
 else:
     import numpy as np
     np.seterr(divide='ignore')
