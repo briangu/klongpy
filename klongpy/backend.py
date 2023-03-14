7f990032f58229d324a9a8c8ab9c0c2591487c17
Brian Guarraci <brian.guarraci@gmail.com>
2023-03-14 16:29:11 -0600

    update kernel

--- klongpy/backend.py
+++ klongpy/backend.py
@@ -42,6 +42,34 @@ if use_gpu:
         }
         ''', 'subtract_reduce_kernel_1d')
 
+    subtract_reduce_kernel_1d = cp.RawKernel(r"""
+template<typename T>
+__global__ void subtract_reduce_kernel_1d_generic(const T* input, T* output, const int length) {
+    extern __shared__ T sdata[];
+    unsigned int tid = threadIdx.x;
+    unsigned int i = blockIdx.x * blockDim.x * 2 + threadIdx.x;
+
+    T subtraction = 0;
+    if (i < length) {
+        subtraction = (i + blockDim.x < length) ? input[i] - input[i + blockDim.x] : input[i];
+    }
+
+    sdata[tid] = subtraction;
+    __syncthreads();
+
+    for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1) {
+        if (tid < s) {
+            sdata[tid] -= sdata[tid + s];
+        }
+        __syncthreads();
+    }
+
+    if (tid == 0) {
+        atomic_subtract(output, sdata[0]);
+    }
+}
+    """)
+
     def subtract_reduce_1(arr):
         input_arr = cp.asarray(arr)
         length = input_arr.size
