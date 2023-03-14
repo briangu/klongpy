64b00b38e88c2cef2db78cfe84083ff4fab2bde7
Brian Guarraci <brian.guarraci@gmail.com>
2023-03-14 17:06:48 -0600

    update kernel

--- klongpy/backend.py
+++ klongpy/backend.py
@@ -42,49 +42,7 @@ if use_gpu:
     #     }
     #     ''', 'subtract_reduce_kernel_1d')
 
-#     subtract_reduce_kernel_1d = cp.RawKernel(r"""
-# template<typename T>
-# __global__ void subtract_reduce_kernel_1d(const T* input, T* output, const int length) {
-#     extern __shared__ T sdata[];
-#     unsigned int tid = threadIdx.x;
-#     unsigned int i = blockIdx.x * blockDim.x * 2 + threadIdx.x;
-
-#     T subtraction = 0;
-#     if (i < length) {
-#         subtraction = (i + blockDim.x < length) ? input[i] - input[i + blockDim.x] : input[i];
-#     }
-
-#     sdata[tid] = subtraction;
-#     __syncthreads();
-
-#     for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1) {
-#         if (tid < s) {
-#             sdata[tid] -= sdata[tid + s];
-#         }
-#         __syncthreads();
-#     }
-
-#     if (tid == 0) {
-#         atomicAdd(output, -sdata[0]);
-#     }
-# }
-# template __global__ void subtract_reduce_kernel_1d<float>(const float* input, float* output, const int length);
-# template __global__ void subtract_reduce_kernel_1d<double>(const double* input, double* output, const int length);
-#     """, "subtract_reduce_kernel_1d")
-
-    kernel_name_map = {
-        "float32": "subtract_reduce_kernel_1d<float>",
-        "float64": "subtract_reduce_kernel_1d<double>"
-    }
-
-    def subtract_reduce_1(arr):
-        input_arr = cp.asarray(arr)
-        length = input_arr.size
-        output_arr = cp.zeros(1, dtype=input_arr.dtype)
-
-        kernel_name = kernel_name_map[input_arr.dtype.name]
-
-        subtract_reduce_kernel_1d = cp.RawKernel(r"""
+    subtract_reduce_kernel_1d = cp.RawKernel(r"""
 template<typename T>
 __global__ void subtract_reduce_kernel_1d(const T* input, T* output, const int length) {
     extern __shared__ T sdata[];
@@ -112,7 +70,49 @@ __global__ void subtract_reduce_kernel_1d(const T* input, T* output, const int l
 }
 template __global__ void subtract_reduce_kernel_1d<float>(const float* input, float* output, const int length);
 template __global__ void subtract_reduce_kernel_1d<double>(const double* input, double* output, const int length);
-    """, kernel_name)
+    """, "subtract_reduce_kernel_1d")
+
+    # kernel_name_map = {
+    #     "float32": "subtract_reduce_kernel_1d<float>",
+    #     "float64": "subtract_reduce_kernel_1d<double>"
+    # }
+
+    def subtract_reduce_1(arr):
+        input_arr = cp.asarray(arr)
+        length = input_arr.size
+        output_arr = cp.zeros(1, dtype=input_arr.dtype)
+
+#         kernel_name = kernel_name_map[input_arr.dtype.name]
+
+#         subtract_reduce_kernel_1d = cp.RawKernel(r"""
+# template<typename T>
+# __global__ void subtract_reduce_kernel_1d(const T* input, T* output, const int length) {
+#     extern __shared__ T sdata[];
+#     unsigned int tid = threadIdx.x;
+#     unsigned int i = blockIdx.x * blockDim.x * 2 + threadIdx.x;
+
+#     T subtraction = 0;
+#     if (i < length) {
+#         subtraction = (i + blockDim.x < length) ? input[i] - input[i + blockDim.x] : input[i];
+#     }
+
+#     sdata[tid] = subtraction;
+#     __syncthreads();
+
+#     for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1) {
+#         if (tid < s) {
+#             sdata[tid] -= sdata[tid + s];
+#         }
+#         __syncthreads();
+#     }
+
+#     if (tid == 0) {
+#         atomicAdd(output, -sdata[0]);
+#     }
+# }
+# template __global__ void subtract_reduce_kernel_1d<float>(const float* input, float* output, const int length);
+# template __global__ void subtract_reduce_kernel_1d<double>(const double* input, double* output, const int length);
+#     """, kernel_name)
 
         threads_per_block = 256
         blocks_per_grid = (length + 2 * threads_per_block - 1) // (2 * threads_per_block)
