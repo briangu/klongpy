5c5f4057ee91d696755197cabe3b3247c2ea6a03
Brian Guarraci <brian.guarraci@gmail.com>
2023-07-21 09:54:21 -0600

    add nested dict test

--- tests/test_extra_suite.py
+++ tests/test_extra_suite.py
@@ -11,6 +11,13 @@ class TestExtraCoreSuite(unittest.TestCase):
     def assert_eval_cmp(self, a, b, klong=None):
         self.assertTrue(eval_cmp(a, b, klong=klong))
 
+    @unittest.skip
+    def test_nested_dict(self):
+        klong = KlongInterpreter()
+        klong('c:::{["GET" :{["/" 2]}]}')
+        r = klong('(c?"GET")?"/"')
+        self.assertEqual(r, 2)
+
     def test_apply_range(self):
         klong = KlongInterpreter()
         r = klong("{x}@,!100")
