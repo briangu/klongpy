9ea73718bd5394f6eddacbc4fdb450f88d273e06
Brian Guarraci <brian.guarraci@gmail.com>
2023-12-13 12:00:50 -0700

    bump version to 0.5.10

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ extra_requires['all'] = extra_requires['full']
 setup(
     name='klongpy',
     packages=['klongpy', 'klongpy.web', 'klongpy.db'],
-    version='0.5.9',
+    version='0.5.10',
     description='Vectorized implementation of Klong language.',
     author='Brian Guarraci',
     license='MIT',
