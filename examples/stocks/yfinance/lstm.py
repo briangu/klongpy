37299bd6ec61f07d057ecfdf9a25e00b8cf4977b
Brian Guarraci <brian@ops5.com>
2023-12-23 10:43:02 -0700

    add draft lstm example

---
+++ examples/stocks/yfinance/lstm.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn as nn
+
+
+class LSTMModel(nn.Module):
+    def __init__(self, input_size, hidden_layer_size, output_size):
+        super(LSTMModel, self).__init__()
+        self.hidden_layer_size = hidden_layer_size
+        self.lstm = nn.LSTM(input_size, hidden_layer_size)
+        self.linear = nn.Linear(hidden_layer_size, output_size)
+
+    def forward(self, input_seq):
+        lstm_out, _ = self.lstm(input_seq.view(len(input_seq), 1, -1))
+        predictions = self.linear(lstm_out.view(len(input_seq), -1))
+        return predictions[-1]
+
+
+def saveModel(model, filename):
+    torch.save(model.state_dict(), filename)
+
+
+def load_model(filename, input_size, hidden_layer_size, output_size):
+    model = LSTMModel(input_size, hidden_layer_size, output_size)
+    model.load_state_dict(torch.load(filename))
+    model.eval()
+    return model
+
+
+def train(model, input_seq, label_seq, epochs):
+    epoch_losses = []
+    loss_function = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    for i in range(epochs):
+        optimizer.zero_grad()
+        y_pred = model(input_seq)
+        single_loss = loss_function(y_pred, label_seq)
+        single_loss.backward()
+        optimizer.step()
+        if i % (max(1,epochs//10)) == 0:
+            epoch_losses.append(single_loss.item())
+            # print(f'epoch: {i:3} loss: {single_loss.item():10.8f}')
+    # print(f'epoch: {i:3} loss: {single_loss.item():10.10f}')
+    return epoch_losses
+
+
+def predict(model, input_seq):
+    return model(input_seq)
+
+
+klong_exports = {"loadModel": load_model, "saveModel": saveModel, "predict": predict}
