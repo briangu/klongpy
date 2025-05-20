ef5ce6a95d31b292bcae2bbeaad9bdaf193f83b9
Brian Guarraci <briang@openai.com>
2025-05-20 07:43:13 -0700

    Expand numpy slice tests and README examples

--- tests/test_numpy_slice.py
+++ tests/test_numpy_slice.py
@@ -11,3 +11,32 @@ class TestNumpySliceBehavior(unittest.TestCase):
 
     def test_index_in_depth_wildcard(self):
         self.assert_eval_cmp('[[1 2 3] [4 5 6]]:@[[] 1]', '[2 5]')
+
+    def test_reshape_wildcard_front(self):
+        """Wildcard as the first dimension"""
+        self.assert_eval_cmp('[[] 2]:^[1 2 3 4 5 6]', '[[1 2] [3 4] [5 6]]')
+
+    def test_index_row_wildcard(self):
+        """Select entire first row using wildcard"""
+        self.assert_eval_cmp('[[1 2 3] [4 5 6]]:@[0 []]', '[1 2 3]')
+
+    def test_index_column_wildcard(self):
+        """Select entire third column using wildcard"""
+        self.assert_eval_cmp('[[1 2 3] [4 5 6]]:@[[] 2]', '[3 6]')
+
+    def test_index_negative_row(self):
+        """Use negative index for last row"""
+        self.assert_eval_cmp('[[1 2 3] [4 5 6]]:@[-1 []]', '[4 5 6]')
+
+    def test_index_negative_column(self):
+        """Use negative index for last column"""
+        self.assert_eval_cmp('[[1 2 3] [4 5 6]]:@[[] -1]', '[3 6]')
+
+    def test_index_entire_matrix(self):
+        """Wildcard for all rows and columns"""
+        self.assert_eval_cmp('[[1 2 3] [4 5 6]]:@[[] []]', '[[1 2 3] [4 5 6]]')
+
+    def test_index_3d_wildcard(self):
+        """Indexing into 3D array with wildcard"""
+        expr = '[[[1 2] [3 4]] [[5 6] [7 8]]]:@[1 0 []]'
+        self.assert_eval_cmp(expr, '[5 6]')
